2880e4d42a52b5a37bd9425701db79e39f064fa9
fuchuan8181 <267451153@qq.com>
2015-12-31 22:05:16 +0800

    ApacheLog

--- src/logic/ApacheLog.java
+++ src/logic/ApacheLog.java
@@ -1,9 +1,21 @@
 package logic;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import gloable.LogDataItem;
+import gloable.MiddleDataVector;
+
 public class ApacheLog implements LogReadIFace{
 
 	public void readAndAnalysis(String fileName) {
 		// TODO Auto-generated method stub
+		File file = new File(fileName);
+		
 		
 	}
 	
